838396f7b185c820f54cd51888297687011f1875
THAINA OLIVEIRA ARAUJO <thaina.oaraujo1@senacsp.edu.br>
2022-11-08 11:56:04 -0300

    csharp

---
+++ AccessSystem/PortariaApp/frmTestarConexao.Designer.cs
@@ -0,0 +1,92 @@
+﻿
+namespace PortariaApp
+{
+    partial class frmTestarConexao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.lblMostrarStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(37, 244);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(252, 194);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar Banco de dados";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesconectar.Location = new System.Drawing.Point(445, 244);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(252, 194);
+            this.btnDesconectar.TabIndex = 1;
+            this.btnDesconectar.Text = "Desconectar Banco de dados";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // lblMostrarStatus
+            // 
+            this.lblMostrarStatus.AutoSize = true;
+            this.lblMostrarStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMostrarStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblMostrarStatus.Location = new System.Drawing.Point(316, 146);
+            this.lblMostrarStatus.Name = "lblMostrarStatus";
+            this.lblMostrarStatus.Size = new System.Drawing.Size(92, 31);
+            this.lblMostrarStatus.TabIndex = 2;
+            this.lblMostrarStatus.Text = "label1";
+            this.lblMostrarStatus.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmTestarConexao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMostrarStatus);
+            this.Controls.Add(this.btnDesconectar);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "frmTestarConexao";
+            this.Text = "frmTestarConexao";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Label lblMostrarStatus;
+    }
+}
---
+++ AccessSystem/PortariaApp/frmTestarConexao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PortariaApp
+{
+    public partial class frmTestarConexao : Form
+    {
+        public frmTestarConexao()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = "Server=localhost;Port=3306;Database=dbPortaria;Uid=root;Pwd=''";
+            try
+            {
+                conn.Open();
+                lblMostrarStatus.Text = "Banco de dados conectado";
+            }catch (MySqlException)
+            {
+                lblMostrarStatus.Text = "Erro ao conectar";
+            }
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.Close();
+            lblMostrarStatus.Text = "Banco de dados desconectado";
+        }
+    }
+}
